a08c1a6fc4b77c6e0f0c57b688cae7e1cf689ae9
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-06-25 22:44:20 +0100

    Add files via upload

--- projectInfo.js
+++ projectInfo.js
@@ -3,7 +3,7 @@ const simpleLedger = {
         title: "SimpleLedger",
         category: "FullStack",
         img: "./images/simpleLedgerPNG.PNG",
-        description: "SimpleLedger is a RESTful API node.js mid-size project which utilises NOSql database mongoDB, as well as Express.js application framework. This app has following features: Entering data which are stored in MongoDB database while displaying them on your screen live. You are also able to remove specific entries using 'X' buttons, or you can remove all entries by pressing reset. Lastly, the data can be exported in CSV format for any further use.",
+        description: "SimpleLedger is a RESTful API node.js mid-size project which utilises NOSql database mongoDB, as well as Express.js application framework. This app has following features: Entering data which are stored in MongoDB database while displaying them on your screen live. You are also able to remove specific entries using 'X' buttons, or you can remove all entries by pressing reset.",
         link:"https://simpleledgerapi.onrender.com",
         githubLink:"https://github.com/zyjaruWeb/portfolio/tree/main/docs/simpleLedgerAPI",
 }
